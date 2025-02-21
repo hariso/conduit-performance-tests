85a04438c416399b4371b20ccf7412100d009087
Haris Osmanagic <haris@meroxa.io>
2025-02-21 16:05:47 +0100

    example records, test data

--- mongodb/infrastructure/insert_test_data.sh
+++ mongodb/infrastructure/insert_test_data.sh
@@ -2,6 +2,19 @@
 
 set -eou pipefail
 
+N=1
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mongosh "mongodb://mongo1:30001,mongo2:30002,mongo3:30003/test?replicaSet=my-replica-set" --eval "load(\"$SCRIPT_DIR/insert-many-users.js\")"
+# Start timer
+SECONDS=0
+
+for ((i=1; i<=N; i++)); do
+  mongosh "mongodb://mongo1:30001,mongo2:30002,mongo3:30003/test?replicaSet=my-replica-set" \
+    --eval "load(\"$SCRIPT_DIR/insert-test-users.js\")" &
+done
+
+# Wait for all background jobs to finish
+wait
+
+# Calculate elapsed time
+echo "All $N parallel executions completed in $SECONDS seconds."
